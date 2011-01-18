48c385916dd41386c0e24cb971b92d8781a6ce18
Ram Singla <ram.singla@gmail.com>
2011-01-18 12:17:55 +0530

    Version changed from Development to 5.8.3 to make sure it works with easytags plugin

--- ctags.h
+++ ctags.h
@@ -15,7 +15,7 @@
 *   MACROS
 */
 #ifndef PROGRAM_VERSION
-# define PROGRAM_VERSION "Development"
+# define PROGRAM_VERSION "5.8.3"
 #endif
 #define PROGRAM_NAME      "Exuberant Ctags"
 #define PROGRAM_URL       "http://ctags.sourceforge.net"
